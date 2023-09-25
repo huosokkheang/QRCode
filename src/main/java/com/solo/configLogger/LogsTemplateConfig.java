b6f10a623124bae666f2369f430e414a160ec56e
SokkheangHUO <sokkheanghuo@gmail.com>
2023-09-25 23:59:56 +0700

    Apply log

---
+++ src/main/java/com/solo/configLogger/LogsTemplateConfig.java
@@ -0,0 +1,10 @@
+package com.solo.configLogger;
+
+import org.springframework.stereotype.Component;
+
+import com.core.liveLog.LogsTemplate;
+
+@Component
+public class LogsTemplateConfig extends LogsTemplate {
+
+}
